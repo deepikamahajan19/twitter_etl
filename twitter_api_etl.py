02c63cb51fc7dab5edfb335ba38c40e03bc06282
deepikamahajan19 <62209806+deepikamahajan19@users.noreply.github.com>
2023-11-30 16:46:13 +0530

    Add files via upload
    importing packages and setting keys

---
+++ twitter_api_etl.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# importing packages
+import pandas as pd
+import s3fs
+from datetime import datetime
+import tweepy
+
+
+# In[5]:
+
+
+# Extracting data from twitter api
+consumer_key="RlpkZI3B0ClQH1nKBkAVyEsJw"
+consumer_secret_key="QrkG3YXhWTWntXtlMJSXl0Xo90hqKHF6QCY7L4aq59V9Zqne6f"
+access_key="1729812541402959873-ZYacgn4mNRfqO9KQIBrF5ccilBNSN7"
+consumer_secret_key="1gGvJCclSaVoQ8loa8Cc8pWXgkcYWHDptxLjic11EEYS4"
+
+
+# In[ ]:
+
+
+# storing it in database or s3 bucket
+
+
+# In[ ]:
+
+
+
+
